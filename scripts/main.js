ad8f182c49e34b2050c6d313d73022ab36a0892c
elabuono <erica.labuono@gmail.com>
2019-02-12 15:35:42 -0500

    Using components in JS
    removed excess space and text while implementing the newer design for moving a ship object

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,73 @@
+// constants
 let ship_x = 10
 let ship_y = 450
 let ship_size = 50
+var playerOne;
 
-// utilize canvas
-var canvas = document.getElementById('gamescreen')
-canvas.width = 500;
-canvas.height = 500;
-ctx = canvas.getContext('2d')
+function launchGame() {
+  playerOne = new component(ship_size, ship_size, ship_x, ship_y);
+  gameScreen.start();
+}
+
+// utilize canvas from html, set interval for components
+var gameScreen = {
+  canvas : document.getElementById('gamescreen'),
+  start : function() {
+    this.canvas.width = 500;
+    this.canvas.height = 500;
+    this.ctx = this.canvas.getContext('2d');
+    this.interval = setInterval(updateGameScreen)
+    document.addEventListener('keypress', handleInput)
+  },
+  clear : function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop : function() {
+    clearInterval(this.interval)
+  }
+}
+
+// first component: the player's ship
+function component(width, height, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.speed = 0;
+  ship_image = new Image();
+  ship_image.src = 'img/ship.png';
+  this.update = function() {
+    ctx = gameScreen.ctx;
+    ctx.drawImage(ship_image, this.x, this.y, this.width, this.height);
+  },
+  this.movePos = function() {
+    this.x += this.speed;
+  }
+}
+
+// interval updates to game screen
+function updateGameScreen() {
+    gameScreen.clear();
+    playerOne.movePos();
+    playerOne.update();
+}
+
+// move ship
+// TODO: require holding down button rather than just constant movement
+function handleInput(event) {
+  const key = event.key
+  if (key === 'a') {
+    if (playerOne.x > 0) {
+      playerOne.speed = -1;
+    }
+  } else if (key === 'd') {
+    if (playerOne.x < 440) {
+      playerOne.speed = 1;
+    }
+  }
+}
+
+
 
 // design a basic movement scheme for the image to move side to side
 function ship_movement() {
@@ -18,21 +79,5 @@ function ship_movement() {
 }
 
 // initialize ship location
-requestAnimationFrame(ship_movement)
-ship_movement()
-
-// move ship
-function handleInput(event) {
-  const key = event.key
-  if (key === 'a') {
-    if (ship_x > 14) {
-      ship_x -= 5
-    }
-  } else if (key === 'd') {
-    if (ship_x < 440) {
-    ship_x += 5
-    }
-  }
-}
-
-document.addEventListener('keypress', handleInput)
+//requestAnimationFrame(ship_movement)
+//ship_movement()
